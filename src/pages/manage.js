7d298eff0fa835c927463ea96db3934e8421f44c
raphaellueckl <codepleb4@gmail.com>
2020-03-15 10:00:09 +0100

    creat initial draft for asset management view

--- src/pages/manage.js
+++ src/pages/manage.js
@@ -8,5 +8,19 @@ export const manageAssetsPage = `
     <div class="text">
         <p>Asset management content</p>
     </div>
+    <div>
+        <label>Asset: <input></label>
+        <label>Symbol: <input></label>
+        <label>Category: <input></label>
+        <label>Amount: <input></label>
+    </div>
+    <ul>
+        <li>
+            <label>Asset: <input></label>
+            <label>Symbol: <input></label>
+            <label>Category: <input></label>
+            <label>Amount: <input></label>
+        </li>
+    </ul>
 </div>
 `;
