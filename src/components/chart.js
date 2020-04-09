b0491bb6c2bf2a668c4c20bff876091b034f1f39
raphaellueckl <codepleb4@gmail.com>
2020-04-09 22:13:17 +0200

    checkin non-working draft for chart connector

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,3 +1,6 @@
+import { store } from "../store.js";
+import { databaseConnector } from "../data/database-connector.js";
+
 const template = document.createElement("template");
 const fullyLoaded = 503;
 template.innerHTML = `
@@ -35,66 +38,117 @@ class Chart extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["content", "title"];
+    return ["content", "title", "chart-subscriber"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "title") {
       this.shadowRoot.querySelector("h2").textContent = newValue;
+    } else if (name === "chart-subscriber") {
+      // INIT
+
+      store.addEventListener(newValue, ({ detail: assetList }) => {
+        debugger;
+        this._drawChart(assetList);
+      });
+      // this._drawChart(databaseConnector.getAssets());
     } else if (name === "content") {
-      try {
-        const values = JSON.parse(newValue);
-        const svg = this.shadowRoot.querySelector("svg");
-        svg.textContent = "";
-
-        const sumOfValues = values
-          .map(v => +v.value)
-          .reduce((a, b) => a + b, 0);
-        console.log("sum", sumOfValues);
-
-        let accumulatedDegree = 0;
-        values.sort((a, b) => b.value - a.value);
-        for (let i = 0; i < values.length; i++) {
-          const weight = (fullyLoaded / sumOfValues) * Number(values[i].value);
-
-          values[i].dashOffset = fullyLoaded - weight;
-
-          const entry = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "circle"
-          );
-          entry.setAttribute("class", "value");
-          entry.setAttribute("cx", "125");
-          entry.setAttribute("cy", "125");
-          entry.setAttribute("r", "80");
-          entry.setAttribute("stroke", getRandomizedHex());
-          entry.setAttribute("stroke-width", "50");
-          entry.setAttribute("fill", "none");
-          entry.setAttribute("stroke-dasharray", fullyLoaded);
-          entry.setAttribute("stroke-dashoffset", values[i].dashOffset);
-          entry.setAttribute(
-            "style",
-            `transform: rotate(${accumulatedDegree}deg);`
-          );
-          entry.setAttribute("title", values[i].label);
-
-          const title = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "title"
-          );
-          title.textContent = values[i].label;
-          entry.appendChild(title);
-          svg.appendChild(entry);
-
-          accumulatedDegree += (360 / fullyLoaded) * weight;
-        }
-      } catch (err) {
-        console.error(err);
-      }
+      // try {
+      //   const values = JSON.parse(newValue);
+      //   const svg = this.shadowRoot.querySelector("svg");
+      //   svg.textContent = "";
+      //   const sumOfValues = values
+      //     .map(v => +v.value)
+      //     .reduce((a, b) => a + b, 0);
+      //   console.log("sum", sumOfValues);
+      //   let accumulatedDegree = 0;
+      //   values.sort((a, b) => b.value - a.value);
+      //   for (let i = 0; i < values.length; i++) {
+      //     const weight = (fullyLoaded / sumOfValues) * Number(values[i].value);
+      //     values[i].dashOffset = fullyLoaded - weight;
+      //     const entry = document.createElementNS(
+      //       "http://www.w3.org/2000/svg",
+      //       "circle"
+      //     );
+      //     entry.setAttribute("class", "value");
+      //     entry.setAttribute("cx", "125");
+      //     entry.setAttribute("cy", "125");
+      //     entry.setAttribute("r", "80");
+      //     entry.setAttribute("stroke", getRandomizedHex());
+      //     entry.setAttribute("stroke-width", "50");
+      //     entry.setAttribute("fill", "none");
+      //     entry.setAttribute("stroke-dasharray", fullyLoaded);
+      //     entry.setAttribute("stroke-dashoffset", values[i].dashOffset);
+      //     entry.setAttribute(
+      //       "style",
+      //       `transform: rotate(${accumulatedDegree}deg);`
+      //     );
+      //     entry.setAttribute("title", values[i].label);
+      //     const title = document.createElementNS(
+      //       "http://www.w3.org/2000/svg",
+      //       "title"
+      //     );
+      //     title.textContent = values[i].label;
+      //     entry.appendChild(title);
+      //     svg.appendChild(entry);
+      //     accumulatedDegree += (360 / fullyLoaded) * weight;
+      //   }
+      // } catch (err) {
+      //   console.error(err);
+      // }
     }
   }
 
-  connectedCallback() {}
+  _drawChart(assetList) {
+    try {
+      const svg = this.shadowRoot.querySelector("svg");
+      svg.textContent = "";
+      debugger;
+      const sumOfValues = assetList
+        .map(v => +v.value)
+        .reduce((a, b) => a + b, 0);
+      console.log("sum", sumOfValues);
+
+      let accumulatedDegree = 0;
+      assetList.sort((a, b) => b.value - a.value);
+      for (let i = 0; i < assetList.length; i++) {
+        const weight = (fullyLoaded / sumOfValues) * Number(assetList[i].value);
+
+        assetList[i].dashOffset = fullyLoaded - weight;
+
+        const entry = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "circle"
+        );
+        entry.setAttribute("class", "value");
+        entry.setAttribute("cx", "125");
+        entry.setAttribute("cy", "125");
+        entry.setAttribute("r", "80");
+        entry.setAttribute("stroke", getRandomizedHex());
+        entry.setAttribute("stroke-width", "50");
+        entry.setAttribute("fill", "none");
+        entry.setAttribute("stroke-dasharray", fullyLoaded);
+        entry.setAttribute("stroke-dashoffset", assetList[i].dashOffset);
+        entry.setAttribute(
+          "style",
+          `transform: rotate(${accumulatedDegree}deg);`
+        );
+        entry.setAttribute("title", assetList[i].label);
+
+        const title = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "title"
+        );
+        title.textContent = assetList[i].label;
+        entry.appendChild(title);
+        svg.appendChild(entry);
+
+        accumulatedDegree += (360 / fullyLoaded) * weight;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 customElements.define("hk-chart", Chart);
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,7 +6,7 @@ export const dashboardPage = `
 </style>
 
 <h1>Dashboard</h1>
-<hk-chart title="Asset Spread" content='[{"label":"BTC", "value":40},{"label":"ETH", "value":60},{"label":"IOTA", "value":10}]'></hk-chart>
+<hk-chart id="assets-spread" chart-subscriber="updated_assets" title="Asset Spread" content='[{"label":"BTC", "value":40},{"label":"ETH", "value":60},{"label":"IOTA", "value":10}]'></hk-chart>
 <div class="content">
     <hk-dashboard></hk-dashboard>
 </div>
