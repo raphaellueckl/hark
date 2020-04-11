ad5059fb349fcc9deea71dfb59c2d5f2a93ab4db
raphaellueckl <codepleb4@gmail.com>
2020-04-11 21:40:14 +0200

    extract chart into own component

--- src/components/chart/asset-spread-chart.js
+++ src/components/chart/asset-spread-chart.js
@@ -5,7 +5,7 @@ import "./chart.js";
 const template = document.createElement("template");
 
 template.innerHTML = `
-  <hk-chart id="assets-spread" chart-subscriber="updated_assets-price" title="Asset Spread" ></hk-chart>`;
+  <hk-chart title="Asset Spread"></hk-chart>`;
 
 const ALL_HEX_VALUES = "0123456789ABCDEF";
 const DEFAULT_STROKE_WIDTH = "50";
@@ -28,112 +28,104 @@ class Chart extends HTMLElement {
     self = this;
   }
 
-  static get observedAttributes() {
-    return ["title", "chart-subscriber"];
+  connectedCallback() {
+    store.addEventListener("updated_assets-price", this._chartUpdater);
   }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === "title") {
-      this.shadowRoot.querySelector("h2").textContent = newValue;
-    } else if (name === "chart-subscriber") {
-      this.subscriberEventListenerKey = newValue;
-      this.subscriberListener = store.addEventListener(
-        this.subscriberEventListenerKey,
-        this._chartUpdater
-      );
-    }
-  }
-
-  disconnectedCallback() {
-    if (this.subscriberEventListenerKey)
-      store.removeEventListener(
-        this.subscriberEventListenerKey,
-        this._chartUpdater
-      );
-  }
-
   _chartUpdater({ detail: assetList }) {
-    try {
-      const svg = self.shadowRoot.querySelector("svg");
-      svg.textContent = "";
-      const sumOfValues = assetList
-        .map(v => +v.value * +v.amount)
-        .reduce((a, b) => a + b, 0);
-
-      let accumulatedDegree = 0;
-      assetList.sort((a, b) => b.value - a.value);
-      for (let i = 0; i < assetList.length; i++) {
-        const weight =
+    const sumOfValues = assetList
+      .map(v => +v.value * +v.amount)
+      .reduce((a, b) => a + b, 0);
+    // try {
+    let chartData = assetList.map(asset => {
+      return {
+        name: asset.asset,
+        value: asset.value,
+        weight:
           (STEPS_UNTIL_FULL_CIRCLE / sumOfValues) *
-          Number(+assetList[i].value * +assetList[i].amount);
+          Number(asset.value * asset.amount)
+      };
+    });
 
-        assetList[i].dashOffset = STEPS_UNTIL_FULL_CIRCLE - weight;
+    //---------------
 
-        const entry = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "circle"
-        );
-        entry.setAttribute("class", "value");
-        entry.setAttribute("cx", "125");
-        entry.setAttribute("cy", "125");
-        entry.setAttribute("r", "80");
-        entry.setAttribute("stroke", getRandomizedHex());
-        entry.setAttribute("stroke-width", DEFAULT_STROKE_WIDTH);
-        entry.setAttribute("fill", "none");
-        entry.setAttribute("stroke-dasharray", STEPS_UNTIL_FULL_CIRCLE);
-        entry.setAttribute("stroke-dashoffset", assetList[i].dashOffset);
-        entry.setAttribute(
-          "style",
-          `transform: rotate(${accumulatedDegree}deg);`
-        );
-        entry.setAttribute("title", assetList[i].asset);
-        entry.percentage = (100 / STEPS_UNTIL_FULL_CIRCLE) * weight;
-        entry.assetName = assetList[i].asset;
+    let accumulatedDegree = 0;
+    chartData.sort((a, b) => b.value - a.value);
 
-        entry.addEventListener("mouseenter", () => {
-          entry.style.strokeWidth = "65";
-          this.assetName.textContent = entry.assetName;
-          this.percentage.textContent = `${entry.percentage.toFixed(1)} %`;
-        });
-        entry.addEventListener("mouseleave", () => {
-          entry.style.strokeWidth = DEFAULT_STROKE_WIDTH;
-          this.assetName.textContent = undefined;
-          this.percentage.textContent = undefined;
-        });
+    self.shadowRoot
+      .querySelector("hk-chart")
+      .setAttribute("data", JSON.stringify({ sumOfValues, data: chartData }));
 
-        const title = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "title"
-        );
-        title.textContent = assetList[i].asset;
-        entry.appendChild(title);
-        svg.appendChild(entry);
+    //   for (let i = 0; i < chartData.length; i++) {
+    //     const entry = document.createElementNS(
+    //       "http://www.w3.org/2000/svg",
+    //       "circle"
+    //     );
+    //     entry.setAttribute("class", "value");
+    //     entry.setAttribute("cx", "125");
+    //     entry.setAttribute("cy", "125");
+    //     entry.setAttribute("r", "80");
+    //     entry.setAttribute("stroke", getRandomizedHex());
+    //     entry.setAttribute("stroke-width", DEFAULT_STROKE_WIDTH);
+    //     entry.setAttribute("fill", "none");
+    //     entry.setAttribute("stroke-dasharray", STEPS_UNTIL_FULL_CIRCLE);
+    //     entry.setAttribute(
+    //       "stroke-dashoffset",
+    //       STEPS_UNTIL_FULL_CIRCLE - chartData[i].weight
+    //     );
+    //     entry.setAttribute(
+    //       "style",
+    //       `transform: rotate(${accumulatedDegree}deg);`
+    //     );
+    //     entry.setAttribute("title", chartData[i].name);
+    //     entry.percentage =
+    //       (100 / STEPS_UNTIL_FULL_CIRCLE) * chartData[i].weight;
+    //     entry.assetName = chartData[i].name;
 
-        accumulatedDegree += (360 / STEPS_UNTIL_FULL_CIRCLE) * weight;
-      }
+    //     entry.addEventListener("mouseenter", () => {
+    //       entry.style.strokeWidth = "65";
+    //       this.assetName.textContent = entry.assetName;
+    //       this.percentage.textContent = `${entry.percentage.toFixed(1)} %`;
+    //     });
+    //     entry.addEventListener("mouseleave", () => {
+    //       entry.style.strokeWidth = DEFAULT_STROKE_WIDTH;
+    //       this.assetName.textContent = undefined;
+    //       this.percentage.textContent = undefined;
+    //     });
 
-      this.assetName = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-      );
-      this.assetName.style.color = "#000000";
-      this.assetName.setAttributeNS(null, "x", "50%");
-      this.assetName.setAttributeNS(null, "y", "50%");
-      this.assetName.setAttributeNS(null, "text-anchor", "middle");
-      svg.appendChild(this.assetName);
+    //     const title = document.createElementNS(
+    //       "http://www.w3.org/2000/svg",
+    //       "title"
+    //     );
+    //     title.textContent = chartData[i].name;
+    //     entry.appendChild(title);
+    //     svg.appendChild(entry);
 
-      this.percentage = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-      );
-      this.percentage.style.color = "#000000";
-      this.percentage.setAttributeNS(null, "x", "50%");
-      this.percentage.setAttributeNS(null, "y", "60%");
-      this.percentage.setAttributeNS(null, "text-anchor", "middle");
-      svg.appendChild(this.percentage);
-    } catch (err) {
-      console.error(err);
-    }
+    //     accumulatedDegree +=
+    //       (360 / STEPS_UNTIL_FULL_CIRCLE) * chartData[i].weight;
+    //   }
+
+    //   this.assetName = document.createElementNS(
+    //     "http://www.w3.org/2000/svg",
+    //     "text"
+    //   );
+    //   this.assetName.style.color = "#000000";
+    //   this.assetName.setAttributeNS(null, "x", "50%");
+    //   this.assetName.setAttributeNS(null, "y", "50%");
+    //   this.assetName.setAttributeNS(null, "text-anchor", "middle");
+    //   svg.appendChild(this.assetName);
+
+    //   this.percentage = document.createElementNS(
+    //     "http://www.w3.org/2000/svg",
+    //     "text"
+    //   );
+    //   this.percentage.style.color = "#000000";
+    //   this.percentage.setAttributeNS(null, "x", "50%");
+    //   this.percentage.setAttributeNS(null, "y", "60%");
+    //   this.percentage.setAttributeNS(null, "text-anchor", "middle");
+    //   svg.appendChild(this.percentage);
+    // } catch (err) {
+    //   console.error(err);
+    // }
   }
 }
 
--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -47,111 +47,106 @@ class Chart extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["title", "chart-subscriber"];
+    return ["title", "data"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "title") {
       this.shadowRoot.querySelector("h2").textContent = newValue;
-    } else if (name === "chart-subscriber") {
-      this.subscriberEventListenerKey = newValue;
-      this.subscriberListener = store.addEventListener(
-        this.subscriberEventListenerKey,
-        this._chartUpdater
-      );
-    }
-  }
-
-  disconnectedCallback() {
-    if (this.subscriberEventListenerKey)
-      store.removeEventListener(
-        this.subscriberEventListenerKey,
-        this._chartUpdater
-      );
-  }
-
-  _chartUpdater({ detail: assetList }) {
-    try {
-      const svg = self.shadowRoot.querySelector("svg");
-      svg.textContent = "";
-      const sumOfValues = assetList
-        .map(v => +v.value * +v.amount)
-        .reduce((a, b) => a + b, 0);
-
-      let accumulatedDegree = 0;
-      assetList.sort((a, b) => b.value - a.value);
-      for (let i = 0; i < assetList.length; i++) {
-        const weight =
-          (STEPS_UNTIL_FULL_CIRCLE / sumOfValues) *
-          Number(+assetList[i].value * +assetList[i].amount);
-
-        assetList[i].dashOffset = STEPS_UNTIL_FULL_CIRCLE - weight;
-
-        const entry = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "circle"
-        );
-        entry.setAttribute("class", "value");
-        entry.setAttribute("cx", "125");
-        entry.setAttribute("cy", "125");
-        entry.setAttribute("r", "80");
-        entry.setAttribute("stroke", getRandomizedHex());
-        entry.setAttribute("stroke-width", DEFAULT_STROKE_WIDTH);
-        entry.setAttribute("fill", "none");
-        entry.setAttribute("stroke-dasharray", STEPS_UNTIL_FULL_CIRCLE);
-        entry.setAttribute("stroke-dashoffset", assetList[i].dashOffset);
-        entry.setAttribute(
-          "style",
-          `transform: rotate(${accumulatedDegree}deg);`
-        );
-        entry.setAttribute("title", assetList[i].asset);
-        entry.percentage = (100 / STEPS_UNTIL_FULL_CIRCLE) * weight;
-        entry.assetName = assetList[i].asset;
-
-        entry.addEventListener("mouseenter", () => {
-          entry.style.strokeWidth = "65";
-          this.assetName.textContent = entry.assetName;
-          this.percentage.textContent = `${entry.percentage.toFixed(1)} %`;
-        });
-        entry.addEventListener("mouseleave", () => {
-          entry.style.strokeWidth = DEFAULT_STROKE_WIDTH;
-          this.assetName.textContent = undefined;
-          this.percentage.textContent = undefined;
-        });
-
-        const title = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "title"
-        );
-        title.textContent = assetList[i].asset;
-        entry.appendChild(title);
-        svg.appendChild(entry);
-
-        accumulatedDegree += (360 / STEPS_UNTIL_FULL_CIRCLE) * weight;
+    } else if (name === "data") {
+      //   this.subscriberEventListenerKey = newValue;
+      //   this.subscriberListener = store.addEventListener(
+      //     this.subscriberEventListenerKey,
+      //     this._chartUpdater
+      //   );
+      let chartData = undefined;
+      try {
+        chartData = JSON.parse(newValue);
+        this._chartUpdater(chartData.data);
+      } catch (e) {
+        console.error("Failed to update chart data.", e);
       }
-
-      this.assetName = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-      );
-      this.assetName.style.color = "#000000";
-      this.assetName.setAttributeNS(null, "x", "50%");
-      this.assetName.setAttributeNS(null, "y", "50%");
-      this.assetName.setAttributeNS(null, "text-anchor", "middle");
-      svg.appendChild(this.assetName);
-
-      this.percentage = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-      );
-      this.percentage.style.color = "#000000";
-      this.percentage.setAttributeNS(null, "x", "50%");
-      this.percentage.setAttributeNS(null, "y", "60%");
-      this.percentage.setAttributeNS(null, "text-anchor", "middle");
-      svg.appendChild(this.percentage);
-    } catch (err) {
-      console.error(err);
     }
+  }
+
+  //   disconnectedCallback() {
+  //     if (this.subscriberEventListenerKey)
+  //       store.removeEventListener(
+  //         this.subscriberEventListenerKey,
+  //         this._chartUpdater
+  //       );
+  //   }
+
+  _chartUpdater(chartData) {
+    const svg = self.shadowRoot.querySelector("svg");
+    let accumulatedDegree = 0;
+    for (let i = 0; i < chartData.length; i++) {
+      const entry = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+      entry.setAttribute("class", "value");
+      entry.setAttribute("cx", "125");
+      entry.setAttribute("cy", "125");
+      entry.setAttribute("r", "80");
+      entry.setAttribute("stroke", getRandomizedHex());
+      entry.setAttribute("stroke-width", DEFAULT_STROKE_WIDTH);
+      entry.setAttribute("fill", "none");
+      entry.setAttribute("stroke-dasharray", STEPS_UNTIL_FULL_CIRCLE);
+      entry.setAttribute(
+        "stroke-dashoffset",
+        STEPS_UNTIL_FULL_CIRCLE - chartData[i].weight
+      );
+      entry.setAttribute(
+        "style",
+        `transform: rotate(${accumulatedDegree}deg);`
+      );
+      entry.setAttribute("title", chartData[i].name);
+      entry.percentage = (100 / STEPS_UNTIL_FULL_CIRCLE) * chartData[i].weight;
+      entry.assetName = chartData[i].name;
+
+      entry.addEventListener("mouseenter", () => {
+        entry.style.strokeWidth = "65";
+        this.assetName.textContent = entry.assetName;
+        this.percentage.textContent = `${entry.percentage.toFixed(1)} %`;
+      });
+      entry.addEventListener("mouseleave", () => {
+        entry.style.strokeWidth = DEFAULT_STROKE_WIDTH;
+        this.assetName.textContent = undefined;
+        this.percentage.textContent = undefined;
+      });
+
+      const title = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "title"
+      );
+      title.textContent = chartData[i].name;
+      entry.appendChild(title);
+      svg.appendChild(entry);
+
+      accumulatedDegree +=
+        (360 / STEPS_UNTIL_FULL_CIRCLE) * chartData[i].weight;
+    }
+
+    this.assetName = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "text"
+    );
+    this.assetName.style.color = "#000000";
+    this.assetName.setAttributeNS(null, "x", "50%");
+    this.assetName.setAttributeNS(null, "y", "50%");
+    this.assetName.setAttributeNS(null, "text-anchor", "middle");
+    svg.appendChild(this.assetName);
+
+    this.percentage = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "text"
+    );
+    this.percentage.style.color = "#000000";
+    this.percentage.setAttributeNS(null, "x", "50%");
+    this.percentage.setAttributeNS(null, "y", "60%");
+    this.percentage.setAttributeNS(null, "text-anchor", "middle");
+    svg.appendChild(this.percentage);
   }
 }
 
