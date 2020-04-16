f0f55b341bca5b747a9889166acca5a182091319
raphaellueckl <codepleb4@gmail.com>
2020-04-16 23:04:10 +0200

    unify headings

--- src/pages/fiat-transactions.js
+++ src/pages/fiat-transactions.js
@@ -5,7 +5,7 @@ export const fiatTransactionsPage = `
 <style>
 </style>
 
-<h2>Fiat Transactions</h2>
+<h1>Fiat Transactions</h1>
 <hk-add-fiat-transaction></hk-add-fiat-transaction>
 <hk-fiat-transaction-list></hk-fiat-transaction-list>
 `;
