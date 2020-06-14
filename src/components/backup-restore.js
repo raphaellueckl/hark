6522cd3fd63c22243f66b9249317e01f67c3ac35
raphaellueckl <codepleb4@gmail.com>
2020-06-14 19:42:10 +0200

    Revert "try workaround for ios file export bug"
    This reverts commit edcde24006bca7bf4fa50f7ad354b4c884b93809.

--- src/components/backup-restore.js
+++ src/components/backup-restore.js
@@ -83,7 +83,6 @@ class ExportApplicationState extends HTMLElement {
       const url = URL.createObjectURL(blob);
       tempElement.href = url;
       tempElement.download = fileName;
-      tempElement.target = "_self";
       document.body.appendChild(tempElement);
       tempElement.click();
 
